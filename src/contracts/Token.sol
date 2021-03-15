7b1fba2ba72c305d55eba859084a0ac94233045d
ryanosaur <ryan.justin.taylor@gmail.com>
2021-03-15 17:20:25 -0400

    adds tests to smart contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
-  string public name = "BDB Coin";
+  string public name = "DApp Token";
+  string public symbol = "DAPP";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+
+  constructor() public {
+    totalSupply = 21000000 * (10 ** decimals);
+  }
 }
